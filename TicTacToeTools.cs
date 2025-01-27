5af9c96256ea78a12502b093fb1e308e79c28674
Blinde16 <blakejlinde@gmail.com>
2025-01-27 13:27:10 -0700

    almost done with tic tac toe tools

--- TicTacToeTools.cs
+++ TicTacToeTools.cs
@@ -3,25 +3,73 @@ using System.Security.Cryptography.X509Certificates;
 
 public class TicTacToeTools
 {
-	public TicTacToeTools()
-	{
-		//ValidateGuess
-		public static ValidateGuess(int boardPosition)
-		{
-			//If the position is taken
-			//Check if the position is in the range 0-8
-			//return true if position is available
+	
+    //ValidateGuess
+    public bool ValidateGuess(int boardPosition, List<string> boardArray)
+    {
 
-		}
-		//Update board
-		public static UpdateBoard(int playerNum, int boardPosition)
-		{
-			//convert playernumber to X or O
-			//input X or O in respective boardposition
-			//Return the Board
-		}
-        //CheckWin
+        bool result = true;
 
+        //Check if the position is in the range 0-8
+        if (boardPosition < 1)
+        {
+            Console.WriteLine("Invalid Position: Pick a position between 1 and 9");
+            result = false;
+        }
+        //Check if the position is in the range 0-8
+        if (boardPosition > 9)
+        {
+            Console.WriteLine("Invalid Position: Pick a position between 1 and 9");
+            result = false;
+        }
+        //Check if the position is a number
+        else if (int.TryParse(boardPosition)
+        {
+            Console.WriteLine("sorry, the guess needs to be a letter");
+            result = false;
+        }
+        //If the position is taken
+        else if (boardArray[boardPosition] != "")
+        {
+            result = false;
+        }
 
+        return result;//return true if position is available
     }
+    //Update board
+    public List<string> UpdateBoard(int playerNum, int boardPosition, List<string> boardArray)
+	{
+        //convert playernumber to X or O
+        if (playerNum == 1)
+        {
+            //update the board to reflect the new boardposition
+            boardArray[boardPosition] = "X";
+        }
+        else (playerNum == 2)
+        {
+            //update the board to reflect the new boardposition
+            boardArray[boardPosition] = "O";
+        }
+        return boardArray;
+		//input X or O in respective boardposition
+		//Return the Board
+	}
+    //CheckWin
+    public int CheckWin(List<string> boardArray)
+    {
+
+        //validate the ways that player 1 can win
+        if (boardArray[0] == "X" && boardArray[1] == "X" && boardArray[2] == "X")
+        {
+            return 1;
+        }
+
+
+        return 
+        //Validate the ways that player 2 can win
+    }
+        //return player number 
+        //return that it's a tie
+        // Game is still playing
+        
 }
