5e5d6f1193de22ace5946dcd457bac5e0f6ae7d3
gabe <gabrielliza012@gmail.com>
2025-01-27 06:31:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string choice = "";
+List<string> list = new List<string>()
+{
+    "a","a","a",
+    "a","a","a",
+    "a","a","a",
+};
+
+Console.WriteLine("Welcome to 4-10's Tic-Tac-Toe!");
+
+Console.WriteLine("Which symbol would you like to choose?\n 1. X\n 2. O");
+
+//choice = verifyChoice(Console.ReadLine());
+
+
+for (int i = 0; i < list.Count; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine();
+    }
+    Console.Write($"|{list[i]}|");
+}
+
